5bf0cc2ac6b6185668b3f8851899d2db2d93008a
Mücahit Gök <mucahit.gok98@gmail.com>
2024-07-09 00:01:25 +0300

    pizza tasarım sayfasına başlandı

--- lib/flutter_dersleri/anasayfa.dart
+++ lib/flutter_dersleri/anasayfa.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dersleri/flutter_dersleri/renkler.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({Key? key}) : super(key: key);
@@ -11,14 +12,47 @@ class _AnasayfaState extends State<Anasayfa> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Pizza"),),
-      body: Center(//yatayda ortalama
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,//dikeyde ortalama
-          children: [
-            Text("Merhaba Dünya", style: TextStyle(fontSize: 50),)
-          ],
-        ),
+      appBar: AppBar(
+        title: const Text("Pizza", style: TextStyle(fontSize: 22),),
+        backgroundColor: anaRenk,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 16, left: 16),
+            child: Text("Beef Cheese", style: TextStyle(fontSize: 36,color: anaRenk,
+                fontWeight: FontWeight.bold)),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Image.asset("resimler/pizza_resimm.jpg",width: 200, height: 200),
+
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextButton(onPressed: (){},
+                  child: Text("Cheese",style: TextStyle(color: yaziRenk1),),
+                  style: TextButton.styleFrom(backgroundColor: anaRenk),
+                ),
+                TextButton(onPressed: (){},
+                  child: Text("Sausage",style: TextStyle(color: yaziRenk1),),
+                  style: TextButton.styleFrom(backgroundColor: anaRenk),
+                ),
+                TextButton(onPressed: (){},
+                  child: Text("Olive",style: TextStyle(color: yaziRenk1),),
+                  style: TextButton.styleFrom(backgroundColor: anaRenk),
+                ),
+                TextButton(onPressed: (){},
+                  child: Text("Pepper",style: TextStyle(color: yaziRenk1),),
+                  style: TextButton.styleFrom(backgroundColor: anaRenk),
+                ),
+                ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/flutter_dersleri/renkler.dart
@@ -0,0 +1,6 @@
+
+import 'dart:ui';
+
+var anaRenk = const Color(0XFFCB2020);
+var yaziRenk1 = const Color(0XFFFFFFFF);
+var yaziRenk2 = const Color(0XFF636363);
